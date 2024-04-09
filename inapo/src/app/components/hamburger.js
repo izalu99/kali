5e7b31e7b66e35fb5863ac090a73e6cc7df8fad9
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-04-08 20:49:07 -0600

    adjusted spacing of the lines to be more centered or aligned

--- inapo/src/app/components/hamburger.js
+++ inapo/src/app/components/hamburger.js
@@ -5,10 +5,10 @@ import { useState } from 'react';
 
 const Hamburger = ({ isOpen }) => {
     return (
-        <div data-testid="hamburger" className="relative w-9 h-7">
+        <div data-testid="hamburger" className="relative w-9 h-7 flex flex-col justify-center space-y-2 mt-1">
             <div id="hamburger" className={`w-full h-1 bg-white transition duration-300 transform ${isOpen ? 'rotate-45 translate-y-3': ''}`}></div>
-            <div id="hamburger" className={`absolute top-3 w-full h-1 bg-white transition duration-300 transform  ${isOpen ? 'opacity-0': ''}`}></div>
-            <div id="hamburger" className={`absolute bottom-0 w-full h-1 bg-white transition duration-300 transform ${isOpen ? '-rotate-45 -translate-y-3': ''}`}></div>
+            <div id="hamburger" className={`w-full h-1 bg-white transition duration-300 transform  ${isOpen ? 'opacity-0': ''}`}></div>
+            <div id="hamburger" className={`w-full h-1 bg-white transition duration-300 transform ${isOpen ? '-rotate-45 -translate-y-3': ''}`}></div>
         </div>
     );
 };
