a32cfed142d8c1cac876dd3f367f0deb2eefbf51
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-08-06 14:06:12 -0600

    re phrasing the headline

--- src/app/components/headline.js
+++ src/app/components/headline.js
@@ -3,9 +3,13 @@
 const Headline = () => {
     return (
         <div className="">
-            <h1 className="text-5xl font-sans">Welcome</h1>
-            <h1 className="text-5xl font-sans">oagt is all about curiosity and discovery; with a little dose of nostalgia.</h1>
+            <h1 className="text-5xl font-sans">kkd short for kankanaey dictionary is all about curiosity, discovery and remembrance.</h1>
             <h1 className="text-5xl font-sans">This is a place for learning.</h1>
+            <h1 className="text-5xl font-sans">This is a place for sharing.</h1>
+            <h1 className="text-5xl font-sans">This is a place for remembering.</h1>
+            <h1 className="text-5xl font-sans">This is a place for you.</h1>
+            <h1 className="text-5xl font-sans">This is a place for learning kankanaey, one of the many languages spoken in the Cordilleras.</h1>
+            <h1 className="text-5xl font-sans">Welcome to kkd.</h1>
       </div>
     );
 };
