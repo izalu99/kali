070f9c6ba663d94230b851268a06e13f86b85a2c
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-08-09 17:20:03 -0600

    changed one of the menu directories

--- components/menu.tsx
+++ components/menu.tsx
@@ -6,7 +6,7 @@ const Menu = () => {
     return (
         <div data-testid="menu" className="fixed right-0 h-full w-1/3 bg-slate-50 shadow-lg p-5">
             <ul className="flex flex-col justify-between space-y-4">
-                <li><Link href="/" className="text-black no-underline">Home</Link></li>
+                <li><Link href="/" className="text-black no-underline">Dictionary</Link></li>
                 <li><Link href="/about" className="text-black no-underline">About</Link></li>
                 <li><Link href="/contact" className="text-black no-underline">Contact</Link></li>
             </ul>
