31dbccd9ed381d86b3063f203d14809a285c683a
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-08-26 20:11:06 -0600

    gqls for connecting ui to db

--- app/(menu)/admin/page.tsx
+++ app/(menu)/admin/page.tsx
@@ -1,25 +1,8 @@
 
 import Search from "@/components/search";
-import WordCard from "@/components/wordCard";
-import TranslationCard from "@/components/translationCard";
 import NewWord from "@/components/newWord";
 
 const adminPage = () => {
-
-    const testWord = {
-        id: "1",
-        text: "wada",
-        type: "adjective",
-        tense: "present",
-        example: "Ai wada?"
-    }
-
-    const testTranslation = {
-        id: "trans-001",
-        text: "there is; the existence of",
-        language: "english",
-        wordId: "1"
-    }
     return (
         <div className='flex flex-col'>
             <h1>Admin Page</h1>      
