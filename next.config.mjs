acbf75c8b8549bb0a745321272fecf31c6621f2e
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-09-13 17:11:32 -0600

    next congfig update4

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers(){
-
-        const devURL = process.env.NEXT_PUBLIC_DEV_URL || 'http://localhost:3000';
-        const prodURL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : devURL;
-
         return [
             {
-                source: '/api/:path*',
+                source: '/(.*)',
                 headers: [
                     {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET, POST, PUT, DELETE, PATCH'
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*'
                     },
-                ]
-            }
-        ]
-    }
+                ],
+            }];
+    },
 };
 
 export default nextConfig;
