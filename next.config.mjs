8c92e41979bb41d8e0149b753e269bf4148dcb28
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-09-13 16:54:01 -0600

    next congfig update2

--- next.config.mjs
+++ next.config.mjs
@@ -10,12 +10,8 @@ const nextConfig = {
                 source: '/api/:path*',
                 headers: [
                     {
-                        key: 'Access-Control-Allow-Credentials',
-                        value: 'true'
-                    },
-                    {
                         key: 'Access-Control-Allow-Origin',
-                        value: prodURL
+                        value: '*'
                     },
                     {
                         key: 'Access-Control-Allow-Methods',
