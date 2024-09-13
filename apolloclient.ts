a8715ae28e1de97d3c0baae8b9a660dc04368834
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-09-13 15:53:09 -0600

    log to see if env var is set right

--- apolloclient.ts
+++ apolloclient.ts
@@ -3,9 +3,13 @@ import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 
 const getGraphqlUri = () => {
     if(process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'development') {
-        return `${process.env.NEXT_PUBLIC_API_URL}/api/graphql`;
+        console.log('development mode');
+        console.log(process.env.NEXT_PUBLIC_API_URL);
+        return `${process.env.NEXT_PUBLIC_API_URL}/api/graphql`;        
     } else{
         const vercelUrl = process.env.VERCEL_URL;
+        console.log('production or preview mode');
+        console.log(vercelUrl);
         return `https://${vercelUrl}/api/graphql`;
     }
 };
