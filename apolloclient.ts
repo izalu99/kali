63068534a30dd7613c7e3d0d5245c921e25d05eb
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-09-13 15:08:36 -0600

    modifications to uri

--- apolloclient.ts
+++ apolloclient.ts
@@ -1,8 +1,23 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
+
+
+const getGraphqlUri = () => {
+    if(process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'development') {
+        return `${process.env.NEXT_PUBLIC_API_URL}/api/graphql`;
+    } else{
+        const vercelUrl = process.env.VERCEL_URL;
+        return `https://${vercelUrl}/api/graphql`;
+    }
+};
+
 
 
 const client = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_API_URL,
+    ssrMode: true,
+    link: createHttpLink({
+        uri: getGraphqlUri(),
+        credentials: 'same-origin',
+    }),
     cache: new InMemoryCache()
 })
 
