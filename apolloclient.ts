32e015e3c8fe855bc88ffa7b182d1660428b05fe
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-09-13 23:52:29 -0600

    edited apolloClient if that resolves it3

--- apolloclient.ts
+++ apolloclient.ts
@@ -1,39 +1,35 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-// Check the current environment and build the appropriate URI
+
+
 const getGraphqlUri = () => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log('Running in development mode');
-    return 'http://localhost:3000/api/graphql'; // Hardcoded localhost for dev mode
-  } else {
-    // In Vercel Preview/Production environments
-    const vercelUrl = process.env.VERCEL_URL;
-    if (!vercelUrl) {
-      throw new Error('GraphQL API URL is not set for the production environment.');
-    }
-    console.log(`GraphQL URI (Vercel): https://${vercelUrl}/api/graphql`);
-    return `https://${vercelUrl}/api/graphql`;
-  }
+    const apiUrl = process.env.VERCEL_URL? process.env.VERCEL_URL : 'http://localhost:3000';
+    console.log('apiUrl: ', `https://{apiUrl}/api/graphql`);
+    return `https://${apiUrl}/api/graphql`;
 };
 
+
 const httpLink = new HttpLink({
-  uri: getGraphqlUri(),
-  credentials: 'same-origin', // Adjust this based on the need for cookies/auth
+    uri: getGraphqlUri(),
+    credentials:'same-origin'
 });
+
 
 const authLink = setContext((_, { headers }) => {
-  return {
-    headers: {
-      ...headers,
-    },
-  };
+    return {
+        headers: {
+            ...headers,
+        }
+    }
 });
 
+
+
 const client = new ApolloClient({
-  ssrMode: typeof window === 'undefined',
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+    ssrMode: typeof window === 'undefined',
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()  
 });
 
 export default client;
